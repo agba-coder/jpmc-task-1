0e493ecce395fbc0aa8aad9466b75a5dbf018004
=Demilade <=demiladebamgboye@gmail.com>
2023-02-27 12:03:10 +0100

    fixed errors in client.py

--- client.py
+++ client.py
@@ -43,15 +43,14 @@ def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
     if price_b == 0:
-        return
-    else:
-        return price_a / price_b
+        return 
+    return price_a / price_b
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
-    for _ in iter(range(N)):
+    for _ in range(N):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
@@ -61,4 +60,4 @@ if __name__ == "__main__":
             prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
+        print("Ratio %s" % (getRatio(prices["ABC"], prices["DEF"])))
